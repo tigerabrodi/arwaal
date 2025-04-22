0888b2dbc7eafa0f8d36b40ce906e5b96502fbfb
Tiger Abrodi <tigerabrodi@gmail.com>
2025-04-22 08:00:10 +0200

    fix jsdom not set

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    setupFiles: ['./test/setup.ts'],
+    setupFiles: ['./src/test/setup.ts'],
+    environment: 'jsdom',
   },
 })
