4e1b665ae9ed141b0002b4b8d99a57763ca9bfe6
Tiger Abrodi <tigerabrodi@gmail.com>
2025-04-25 21:08:48 +0200

    lib

---
+++ src/lib.ts
@@ -0,0 +1,11 @@
+import { createElement } from './createElement'
+import { render } from './render'
+import { useState } from './useState'
+
+export { createElement, render, useState }
+
+export const Arwaal = {
+  createElement,
+  render,
+  useState,
+}
